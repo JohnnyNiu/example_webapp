822925c9829b561b26fc274728ccc1dcb024cfcc
JohnnyNiu <johnny.x.niu@gmail.com>
2017-09-04 12:54:48 +1000

    add a controller method to handle post method

--- python_flask/app/views.py
+++ python_flask/app/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import request, render_template
 from app import app
 
-@app.route('/todo/create')
-def createTodoItem():
+@app.route('/todo/create', methods=['GET'])
+def createTodoItemForm():
     return render_template('createItem.html', name="niu")
+
+@app.route('/todo/create', methods=['POST'])
+def createTodoItem():
+    return request.form['content']
