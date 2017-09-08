0bb2382a52f16f41aebc71a340352090a0c6ad87
JohnnyNiu <johnny.x.niu@gmail.com>
2017-09-08 22:52:03 +1000

    improve page flow between create and list items

--- python_flask/app/views.py
+++ python_flask/app/views.py
@@ -1,4 +1,4 @@
-from flask import request, render_template
+from flask import request, render_template, redirect
 from app import app
 from app import itemService
 from app.model.item import Item 
@@ -12,7 +12,7 @@ def createTodoItem():
     itemContent = request.form['content']
     toSave = Item(content=itemContent)
     itemService.saveToDoItem(toSave)
-    return request.form['content']
+    return redirect('/todo/list')
 
 @app.route('/todo/list', methods=['GET'])
 def listTodoItems():
